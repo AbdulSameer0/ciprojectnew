74347713f56b883e10a5ff21f7776dec093af806
Ritika <ritika3225@gmail.com>
2024-04-05 14:20:39 +0530

    All files

---
+++ application/controllers/Prac.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Prac extends CI_Controller {
+
+	
+	public function index()
+	{
+		// $this->load->view('welcome_message');
+        echo "Hello this is paractice session";
+	}
+}
